d56e23b64906609985c147275a845c8d60b08046
moose <moosecodes@gmail.com>
2023-04-21 13:00:56 -0700

    add material icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import singleSpaVue from 'single-spa-vue';
 
 import App from './App.vue';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
+
 const vueLifecycles = singleSpaVue({
   createApp,
   appOptions: {
@@ -19,6 +39,7 @@ const vueLifecycles = singleSpaVue({
       });
     },
   },
+  handleInstance: (app) => { app.use(vuetify) },
 });
 
 export const bootstrap = vueLifecycles.bootstrap;
