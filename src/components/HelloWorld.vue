5f8e258fc5acca947ea4b014588a3291276fe851
moose <moosecodes@gmail.com>
2023-04-21 17:29:01 -0700

    update nav

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="text-center">
-    <div>
       <v-btn
           class="ma-2"
           color="primary"
@@ -22,58 +21,6 @@
             icon="mdi-cancel"
         ></v-icon>
       </v-btn>
-
-      <v-btn
-          class="ma-2"
-      >
-        <v-icon
-            start
-            icon="mdi-minus-circle"
-        ></v-icon>
-        Cancel
-      </v-btn>
-    </div>
-
-    <div>
-      <v-btn
-          class="ma-2"
-          color="orange-darken-2"
-      >
-        <v-icon
-            start
-            icon="mdi-arrow-left"
-        ></v-icon>
-        Back
-      </v-btn>
-
-      <v-btn
-          class="ma-2"
-          color="purple"
-          icon="mdi-wrench"
-      ></v-btn>
-
-      <v-btn
-          class="ma-2"
-          color="indigo"
-          icon="mdi-cloud-upload"
-      ></v-btn>
-    </div>
-
-    <div>
-      <v-btn
-          class="ma-2"
-          variant="text"
-          icon="mdi-thumb-up"
-          color="blue-lighten-2"
-      ></v-btn>
-
-      <v-btn
-          class="ma-2"
-          variant="text"
-          icon="mdi-thumb-down"
-          color="red-lighten-2"
-      ></v-btn>
-    </div>
   </div>
 </template>
 
